337d0d3a063e371b8cc69aadf9502cee5ac7483f
Luca-Nobili <102580302+Luca-Nobili@users.noreply.github.com>
2022-04-11 16:56:39 +0200

    js

---
+++ m1/w3/d1/js/js/naming.js
@@ -0,0 +1,54 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var pet = 'gatto';
+document.write(pet);
+
+var Pet = 'cane'
+document.write(  ' ' );
+console.log(Pet)
+
+
+
+
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
+var _12 = 'numero'; 
+document.write( ' ' + _12)
+
+var roditore = document.getElementById('coniglio');
+console.log('roditore');
+
+console.log(roditore.innerHTML);
+
+roditore.innerHTML = 'falco';
+roditore.style.backgroundColor = 'white';
+
+var oblect = {
+    nome: 'Luca',
+    cognome:'Nobili',
+    eta: 30 
+};
+
+console.log(Object);
+
+document.write('<ul>')
+document.write('<li>' + oblect.nome + '</li>')
+document.write('<li>' + oblect.cognome + '</li>')
+document.write('<li>' + oblect.eta + '</li>')
+document.write('</li>')
+
+console.log('object');
