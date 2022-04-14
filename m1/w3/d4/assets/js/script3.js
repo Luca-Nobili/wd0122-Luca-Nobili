3bf9527651b468a5f04ed7c697c503b9c1c3110f
Luca-Nobili <102580302+Luca-Nobili@users.noreply.github.com>
2022-04-14 18:13:12 +0200

    jsd4

---
+++ m1/w3/d4/assets/js/script3.js
@@ -0,0 +1,45 @@
+let abitazione = 'appartamneto'
+let abitazione1 = 'casa'
+let abitazione2 = 'villa'
+
+console.log(abitazione, abitazione1, abitazione2);
+
+var abitazioni = ['appartamento', 'casa', 'villa']
+console.log(abitazioni);
+
+var sono = 'Ciao sono Luca Nobili'
+var maiuscolo = sono.toLocaleUpperCase()
+console.log(maiuscolo);
+
+var minuscolo = sono.toLocaleLowerCase()
+console.log(minuscolo);
+
+
+
+let arr = [
+    'Giovanni',
+    'Paolo',
+    'Franco',
+    'Angela',
+    'Francesco'
+
+]
+
+let nomi = arr
+console.log(arr.length);
+
+let nome = arr [2]
+console.log(nome.length); 
+
+let elimina = arr.pop()
+console.log(elimina);
+
+let aggiungi = arr.push('Federico')
+console.log(arr);
+
+var elimina2 = arr.shift()
+console.log(elimina2);
+
+var aggiungi2 = arr.unshift(' Massimiliano' )
+console.log(arr);
+
