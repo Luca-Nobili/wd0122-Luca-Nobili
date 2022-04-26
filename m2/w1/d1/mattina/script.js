af811299213f26acbf3a87dbd91fab9ecfdf1529
Luca-Nobili <102580302+Luca-Nobili@users.noreply.github.com>
2022-04-26 16:29:58 +0200

    m2-w1-d1

---
+++ m2/w1/d1/mattina/script.js
@@ -0,0 +1,20 @@
+
+let frutto = 'arance'
+
+
+switch (frutto) {
+   case'banana':
+   console.log('le banane costano 22 al kg');
+   break;
+   case'mele':
+   console.log('le mele costano 20 al kg');
+   break;
+   case'manghi':
+   console.log('posso fare qualcosa se voglio');
+   case'papaye':
+   console.log('i manghi e le papaye costano 30 al kg');
+   break;
+   default:
+       console.log('siamo spaicenti, non disponanimo di '  + frutto);
+
+}
