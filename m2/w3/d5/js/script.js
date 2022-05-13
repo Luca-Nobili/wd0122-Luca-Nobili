eea5bdc4487255d8a1c9c6ae3ca4e6c9b21b58d4
Luca-Nobili <102580302+Luca-Nobili@users.noreply.github.com>
2022-05-13 18:33:47 +0200

    m2-w3-d5

--- m2/w3/d5/js/script.js
+++ m2/w3/d5/js/script.js
@@ -9,7 +9,7 @@ fetch(APPURL)
 console.log(res);
 
 
-let campiVisualizzati = ['username','firstName','lastName','gender','email']
+let campiVisualizzati = ['username','firstName','lastName','gender','email','photo']
 
 let trH = document.createElement('tr')
 for(campo of campiVisualizzati){
@@ -23,22 +23,26 @@ tabella.append(trH)
 for(let utente of res){
 
     let tr = document.createElement('tr')
-
+    let tdPhoto = document.createElement("td");
+    
     for(let prop in utente){
 
         if(campiVisualizzati.includes(prop)){
             let key = prop;
             let value = utente[prop];
 
-
             let td = document.createElement('td')
-
+            
+            tdPhoto.innerHTML = `<img src="${utente.profileURL}" "width=60px height=60px>`;
             td.innerHTML = value
 
+            
 
 
 
-            tr.append(td)
+
+            tr.append(td,tdPhoto)
+          
 }
 }
 
