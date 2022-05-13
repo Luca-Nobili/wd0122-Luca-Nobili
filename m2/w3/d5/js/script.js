34e822ae9642f384f23ed2e0be08adeff1cb18d9
Luca-Nobili <102580302+Luca-Nobili@users.noreply.github.com>
2022-05-13 17:05:29 +0200

    m2-w3-d5

---
+++ m2/w3/d5/js/script.js
@@ -0,0 +1,53 @@
+const APPURL = 'users.json'
+
+let tabella = document.querySelector('#utenti')
+
+fetch(APPURL)
+.then(res => res.json())
+.then(res => {
+
+console.log(res);
+
+
+let campiVisualizzati = ['username','firstName','lastName','gender','profileURL','email']
+
+let trH = document.createElement('tr')
+for(campo of campiVisualizzati){
+let th = document.createElement('th')
+th.innerHTML = campo
+trH.append(th)
+}
+tabella.append(trH)
+
+
+for(let utente of res){
+
+    let tr = document.createElement('tr')
+
+    for(let prop in utente){
+
+        if(campiVisualizzati.includes(prop)){
+            let key = prop;
+            let value = utente[prop];
+
+
+            let td = document.createElement('td')
+
+            td.innerHTML = value
+
+
+
+
+            tr.append(td)
+}
+}
+
+
+tabella.append(tr)
+
+
+}
+
+})
+
+
